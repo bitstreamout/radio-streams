7fd669ab4f4b17b15ea230fa50af3be6f45ed1a2
Werner Fink <werner@suse.de>
2024-01-11 13:04:21 +0100

    Use shebang
    Signed-off-by: Werner Fink <werner@suse.de>

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ import setuptools
 print(setuptools.find_packages("src"))
 
 setuptools.setup(name="radiostreams",
-                 version="0.1",
+                 version="0.2",
                  description="Audio streaming proxy.",
                  author='Sven Festersen',
                  package_dir={"":"src"},
                  packages=setuptools.find_packages("src"),
-                 install_requires=["tornado >=5.0", "pycurl >=7.0"],
+                 install_requires=["tornado >=6.0", "pycurl >=7.0"],
                  scripts=["scripts/stream-proxy"])
